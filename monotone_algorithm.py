880eb86f83f200a9f4638153f105a499778691ff
Avinash Malik <avinash.malik@auckland.ac.nz>
2015-05-06 15:15:01 +1200

    Added the algorithm to check monotone functions. Need to debug it

---
+++ monotone_algorithm.py
@@ -0,0 +1,32 @@
+# Example of the water-tank system odes whose witness functions need to
+# be monotone!
+
+from sympy import *
+
+t = Symbol('t')
+x = Function('x')
+
+# The odes in the water-tank example
+K = Symbol('K')
+h = Symbol('h')
+
+odes = [dsolve(diff(x(t))-K*(h-x(t)), x(t)),
+        dsolve(diff(x(t)), x(t)),
+        dsolve(diff(x(t))+K*x(t), x(t)),
+        dsolve(diff(x(t)), x(t))]
+
+# Debug
+map(lambda x: pprint(x), odes)
+
+# FIXME: There is no error checking here!
+# Differentiate the symbolic solutions to get the slope
+slopes = map(lambda x: x.rhs.diff(t), odes)
+
+# Get the roots of the symbolic functions
+ismonotones = map(lambda x: solve(x, t), slopes)
+
+# Can the HA be solved?
+if (reduce(lambda y, x: (x == []) and y, ismonotones, True) == True):
+    print "All odes in HA are monotone"
+else:
+    print "HA cannot be solved"
