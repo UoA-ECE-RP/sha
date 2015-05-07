514312e28217a49b8444cac4115a9035040f75eb
Avinash Malik <amalik@scss.tcd.ie>
2015-05-07 19:59:44 +1200

    updated example with corret typing

--- examples/watertank.py
+++ examples/watertank.py
@@ -15,30 +15,30 @@ ode3 = Ode(sympify("diff(x(t))+100*x(t)"), sympify("x(t)"), 100)
 ode4 = Ode(sympify("diff(x(t))"), sympify("x(t)"), 20)
 
 # The locations of the hybrid automaton
-t1 = Loc("t1", [ode1], [], sympify("x>=20 & x<=100"))
-t2 = Loc("t2", [ode2], [], sympify("x>=100 & x<=100"))
-t3 = Loc("t3", [ode3], [], sympify("x>=20 & x<=100"))
-t4 = Loc("t4", [ode4], [], sympify("x>=20 & x<=20"))
+t1 = Loc("t1", [ode1], [], Guard(sympify("x>=20 & x<=100")))
+t2 = Loc("t2", [ode2], [], Guard(sympify("x>=100 & x<=100")))
+t3 = Loc("t3", [ode3], [], Guard(sympify("x>=20 & x<=100")))
+t4 = Loc("t4", [ode4], [], Guard(sympify("x>=20 & x<=20")))
 
 # The edges
-e1 = Edge(t1, t2, sympify("x>=100 & x<=100"),
+e1 = Edge(t1, t2, Guard(sympify("x>=100 & x<=100")),
           [Update.Update2(Symbol('x'), Symbol('x'))],
-          Event("B"))
-e2 = Edge(t2, t3, sympify("True"),
+          [Event("B")])
+e2 = Edge(t2, t3, Guard(sympify("True")),
           [Update.Update2(Symbol('x'), Symbol('x'))],
-          Event("OFF"))
-e3 = Edge(t1, t3, sympify("True"),
+          [Event("OFF")])
+e3 = Edge(t1, t3, Guard(sympify("True")),
           [Update.Update2(Symbol('x'), Symbol('x'))],
-          Event("OFF"))
-e4 = Edge(t3, t1, sympify("True"),
+          [Event("OFF")])
+e4 = Edge(t3, t1, Guard(sympify("True")),
           [Update.Update2(Symbol('x'), Symbol('x'))],
-          Event("ON"))
-e5 = Edge(t3, t4, sympify("x>=20 & x<=20"),
+          [Event("ON")])
+e5 = Edge(t3, t4, Guard(sympify("x>=20 & x<=20")),
           [Update.Update2(Symbol('x'), Symbol('x'))],
-          Event("C"))
+          [Event("C")])
 e6 = Edge(t3, t1, sympify("True"),
           [Update.Update2(Symbol('x'), Symbol('x'))],
-          Event("ON"))
+          [Event("ON")])
 
 waterTank = Ha([t1, t2, t3, t4], t4, [e1, e2, e3, e4, e5, e6])
 
