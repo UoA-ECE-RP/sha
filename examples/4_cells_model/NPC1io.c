8d8fe03118bb718545fd42075d483dc61facc642
Théo Steger <steger@ensta.fr>
2015-06-02 16:27:10 +1200

    Example of 4 cells network

---
+++ examples/4_cells_model/NPC1io.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<string.h>
+#include<assert.h>
+
+/* The files are in csv format */
+#define OFILE "file7out.csv"
+#define IFILE "file5in.csv"
+
+#define TRUE 1
+#define FALSE 0
+
+extern double x;
+
+/* The step size */
+double d = 0.2;
+
+/* The events to read from file */
+unsigned char VS, VSP;
+double g;
+
+/* The tick counter */
+/* It is static to hide it inside this file */
+static size_t tick = 0;
+
+/* The output file pointer */
+FILE *fo = NULL;
+
+/* The input file pointer */
+FILE *fi = NULL;
+
+static inline unsigned char getValue(unsigned char t, char* e){
+  assert(t == TRUE || t == FALSE);
+  return t;
+}
+
+/* Read input x from file */
+void readInput() {
+  static unsigned char count = 0;
+  /* The check here is very expensive! */
+  if(0 == count) {
+    fi = fopen(IFILE, "r");
+    if (fi == NULL){
+      perror(IFILE);
+      exit(1);
+    }
+    ++count;
+  }
+  
+  /* The format of input is 1 line/tick */
+  /* for the events above the format is: */
+  /*
+    ON, ON_Value, C, C_Value, B, B_Value, OFF, OFF_Value
+  */
+  char in[256];
+  if (fgets(in,255,fi) == NULL){
+    VS = FALSE;
+    VSP = FALSE;
+  }
+  else{
+    char *ret = NULL, *v = NULL;
+    ret = strtok(in, ",");
+    if(ret == NULL){
+      /* This means nothing found! */
+      /* Set all events to false */
+      VS = FALSE;
+      VSP = FALSE;
+    }
+    while(ret != NULL) {
+      v = strtok(NULL, ",");
+      if (v != NULL) {
+	if (strcmp(ret, "VS") == 0)
+	  VS = getValue((unsigned char)atoi(v), ret);
+	else if(strcmp(ret, "VSP") == 0)
+	  VSP = getValue((unsigned char)atoi(v), ret);
+  else if(strcmp(ret, "g") == 0)
+    g = atof(v);
+      }
+      else {
+	perror("NULL while scanning input");
+	exit(1);
+      }
+      /* Read the next one! */
+      ret = strtok(NULL, ",");
+    }
+  }
+  fprintf(stdout, "%s:%d %s:%d %s:%f tick:%ld\n", "VS", VS,
+	  "VSP", VSP, "g", g, tick);
+  fflush(stdout);
+}
+
+/* Write output x to file */
+void writeOutput(){
+  static unsigned char count = 0;
+  if (0 == count){
+    fo = fopen(OFILE, "w");
+    if (fo == NULL){
+      perror(OFILE);
+      exit(1);
+    }
+    ++count;
+  }
+  fprintf(fo, "%ld,%f\n", ++tick, x);
+}
+
---
+++ examples/4_cells_model/cell1D.c
@@ -0,0 +1,212 @@
+#include<stdint.h>
+
+#include<stdlib.h>
+
+#include<stdio.h>
+
+#define True 1
+
+#define False 0
+
+//Events
+
+extern unsigned char VSP, VS;
+extern double g;
+
+extern void readInput();
+
+extern void writeOutput();
+
+//Continous variables
+
+double x;
+
+//Continous variable update
+
+double x_u;
+
+//The constant variable
+
+double C1;
+
+//Step-size constant d
+
+extern double d;
+
+//Nstep constant k
+
+double k;
+
+//States
+
+enum states {t1 , t2 , t3 , t4};
+
+//Previous state variable
+
+#include "t1.h"
+#include <math.h>
+double t1_ode_1(double C1, double d, double k) {
+   double t1_ode_1_result;
+   t1_ode_1_result = C1*exp(-0.1*d*k)-g*d*k;
+   return t1_ode_1_result;
+}
+double t1_init_1(double x_u) {
+   double t1_init_1_result;
+   t1_init_1_result = x_u;
+   return t1_init_1_result;
+}
+
+
+#include "t2.h"
+#include <math.h>
+double t2_ode_1(double C1, double d, double k) {
+   double t2_ode_1_result;
+   t2_ode_1_result = C1 + 800*d*k-g*d*k;
+   return t2_ode_1_result;
+}
+double t2_init_1(double x_u) {
+   double t2_init_1_result;
+   t2_init_1_result = x_u;
+   return t2_init_1_result;
+}
+
+
+#include "t3.h"
+#include <math.h>
+double t3_ode_1(double C1, double d, double k) {
+   double t3_ode_1_result;
+   t3_ode_1_result = C1*exp(200.0*d*k);
+   return t3_ode_1_result;
+}
+double t3_init_1(double x_u) {
+   double t3_init_1_result;
+   t3_init_1_result = x_u;
+   return t3_init_1_result;
+}
+
+
+#include "t4.h"
+#include <math.h>
+double t4_ode_1(double C1, double d, double k) {
+   double t4_ode_1_result;
+   t4_ode_1_result = C1*exp(-0.002*d*k);
+   return t4_ode_1_result;
+}
+double t4_init_1(double x_u) {
+   double t4_init_1_result;
+   t4_init_1_result = x_u;
+   return t4_init_1_result;
+}
+
+
+enum states cell1D(enum states cstate, enum states pstate) {
+  switch (cstate) {
+  case (t1):
+    if(x <= 20 && !VSP && !VS){
+      if (pstate != cstate)
+        C1 = t1_init_1(x);
+      x_u = t1_ode_1(C1, d, k);
+      if(x_u < 20 && C1 > 20)
+        x_u = 20;
+      ++k;
+      cstate = t1;
+    }
+    else if(VS && !VSP && x < 20) {
+      k=0;
+      cstate=t2;
+      x_u = x;
+    }
+    else if(!VSP && !VS && x >= 20) {
+      k=0;
+      cstate=t3;
+      x_u = x;
+    }
+    else {
+      fprintf(stderr,"Unreachable state!");
+      exit(1);
+    }
+    break;
+  case (t2):
+    if(x < 20 && !VSP && !VS){
+      if (pstate != cstate)
+        C1 = t2_init_1(x);
+      x_u = t2_ode_1(C1, d, k);
+      if(x_u > 20 && C1 < 20)
+        x_u = 20;
+      ++k;
+      cstate = t2;
+    }
+    else if(VSP && !VS && x < 20) {
+      k=0;
+      cstate=t1;
+      x_u = x;
+    }
+    else if(!VSP && !VS && x <= 20 && x >= 20) {
+      k=0;
+      cstate=t3;
+      x_u = x;
+    }
+    else {
+      fprintf(stderr,"Unreachable state!");
+      exit(1);
+    }
+    break;
+  case (t3):
+    if(x >= 20 && x < 138 && !VSP && !VS){
+      if (pstate != cstate)
+        C1 = t3_init_1(x);
+      x_u = t3_ode_1(C1, d, k);
+      if(x_u > 138 && C1 < 138)
+        x_u = 138;
+      ++k;
+      cstate = t3;
+    }
+    else if(!VSP && !VS && x <= 138 && x >= 138) {
+      k=0;
+      cstate=t4;
+      x_u = x;
+    }
+    else {
+      fprintf(stderr,"Unreachable state!");
+      exit(1);
+    }
+    break;
+  case (t4):
+    if(x > 20 && x <= 138 && !VSP && !VS){
+      if (pstate != cstate)
+        C1 = t4_init_1(x);
+      x_u = t4_ode_1(C1, d, k);
+      if(x_u < 20 && C1 > 20)
+        x_u = 20;
+      ++k;
+      cstate = t4;
+    }
+    else if(!VSP && !VS && x <= 20 && x >= 20) {
+      k=0;
+      cstate=t1;
+      x_u = x;
+    }
+    else {
+      fprintf(stderr,"Unreachable state!");
+      exit(1);
+    }
+    break;
+  default: exit(1);
+  }
+  return cstate;
+}
+
+int main(void) {
+  x = 0.0001;
+  enum states pstate = -1;
+  enum states cstate = t1;
+  while(True) {
+    readInput();
+    enum states rstate = cell1D(cstate, pstate);
+    pstate = cstate;
+    cstate = rstate;
+    x = x_u;
+    writeOutput();
+  }
+  return 0;
+}
