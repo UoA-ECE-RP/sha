1d8b94464a0a22b63f57797a7d711e4c839b6185
Avinash Malik <avinash.malik@auckland.ac.nz>
2015-06-18 11:03:40 +1200

    Fixed stupid error

--- language/gen.py
+++ language/gen.py
@@ -659,7 +659,7 @@ def updateLocNsteps(excludes, loc):
                 return Loc(n, odes, cf, invariants, time=tts[0])
             else:
                 raise Exception("Not a WHA!!: " + str(times))
-        elif not ALL_BETS_OFF:
+        elif ALL_BETS_OFF:
             pass
         else:
             raise Exception("Not a WHA!!: " + str(times))
