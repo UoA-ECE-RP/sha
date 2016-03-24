e0d82325b2c708c3f208076682a8a5e33ea11519
lwan353 <lwan353@aucklanduni.ac.nz>
2016-03-25 00:29:53 +1300

    change the way to generate the ode function (discrete)

--- language/shac.py
+++ language/shac.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import macropy.activate
 import gen
+import genChanged1
 import sys
 import compose
 import hajson
@@ -12,18 +13,23 @@ def comp(haList):
 
 def compile(ha, **kwargs):
     # Step-1: Make sure that the hybrid automaton is well-formed
-    if 'COMPOSED' not in kwargs:
-        ha = gen.preprocess(ha)
+    if 'COMPOSED' not in kwargs and 'Q' not in kwargs:
+        #ha = gen.preprocess(ha)
+        ha = genChanged1.preprocess(ha)
     if 'ABOF' in kwargs and kwargs['ABOF']:
-        gen.ALL_BETS_OFF = True
-    if gen.isWha(ha):
+        #gen.ALL_BETS_OFF = True
+        genChanged1.ALL_BETS_OFF = True
+    #if gen.isWha(ha):
+    if genChanged1.isWha(ha):
         # Step-2 get a new ha with the Nsteps for each loc computed
         # FIXME: Nsteps might be None in the general case
-        sha = gen.getSha(ha)
+        #sha = gen.getSha(ha)
+        sha = genChanged1.getSha(ha)
         if sha is None:
             raise "Cannot generate code!"
         else:
-            gen.codeGen(sha)
+            #gen.codeGen(sha)
+            genChanged1.codeGen(sha)
 
 
 def main(argv):
