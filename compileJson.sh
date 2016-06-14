d8c67c36844c07acdb9458747f9f6531211897a3
Jin Chen <chenjin10@gmail.com>
2016-06-14 20:54:26 +1200

    Added instructions into bash files.

--- compileJson.sh
+++ compileJson.sh
@@ -2,7 +2,12 @@
 
 cd ./language
 export PYTHONPATH=`pwd`
+
+#on windows
+#the first path is the path of the json file, and the second path is where you want to save the converted .fbt file
 #python shac.py ../examples/hajson/waterTank.json fbt C:\\vsplugin\\sha\\generatedHA\\
 
+#on linux or mac
+#the first path is the path of the json file, and the second path is where you want to save the converted .fbt file
 python shac.py ../examples/hajson/waterTank.json fbt /Users/jinchen/Desktop/2016_Semester1/VS\ plugin/JsonToIEC/generatedHA/
 python shac.py ../examples/hajson/gasBurner.json fbt /Users/jinchen/Desktop/2016_Semester1/VS\ plugin/JsonToIEC/generatedHA/
--- compileJsonToCFB.sh
+++ compileJsonToCFB.sh
@@ -3,4 +3,5 @@
 cd ./language
 export PYTHONPATH=`pwd`
 
+#the json file should be put in the language folder for now, and the last parameter is the path you want to save the generated .cfb file
 python shac.py gasBurner_waterTank_Interface.json cfb /Users/jinchen/Desktop/2016_Semester1/VS\ plugin/JsonToIEC/generatedHA/
